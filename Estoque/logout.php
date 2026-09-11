4b3aa31042ac575ee091582830c3adc43bb48fcc
Yuri <yuria049@gmail.com>
2026-09-11 14:07:37 -0300

    Delete Estoque directory

--- Estoque/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ============================================================
-// LOGOUT DO SISTEMA (Encerra a sessão e volta para o login)
-// ============================================================
-session_start();
-session_destroy();
-header("Location: login.php");
-exit;
-?>
